e8a81bca44e760ec83f68a2b66c72c7f9ef93b55
LeonardoAC <leo_carrilho@hotmail.com>
2023-10-03 11:34:17 -0300

    2nd commit

--- script.js
+++ script.js
@@ -2,31 +2,51 @@
 function getTimeAndSetTheBackground(){
     /*  
         Se for entre 06:00 e 17:59 fundo será claro
-        Se for entre 18:01 e 05:59 fundo será escuro
+        Se for entre 18:00 e 05:59 fundo será escuro
         funcao chamada ao carregar a paginas
     */
+    
+    // Pega a hora local
     let date = new Date();
     const hour = date.getHours();
-    const minute = date.getMinutes();
-    //console.log('Hora: '+hour+':'+minute+' - bacground: ');
-    //objeto = document.getElementById('');
-    if (inRange(hora, 6, 17) && inRange(minute,1,59)){
-        console.log('background dia');
+    console.log('Hora: '+hour);
+    if ( hour >=6 && hour <=17 ){
+        console.log('getTimeAndSetTheBackground: dia');
+        ChangeBackground('dia');
     } else {
-        console.log('background noite');
+        console.log('getTimeAndSetTheBackground: noite');
+        ChangeBackground('noite');
     }
-    
 }
 
-function ChangeBackground(){
-    /*
-        Troca a imagem de fundo da página, ao deslizar o botao slider
-    */
-   objeto = document.getElementById('btnSlider1').value;
-    if (objeto == 1){
-        console.log('background dia');
+function ChangeBackground(argDiaOuNoite){
+    /* Troca a imagem de fundo da página, ao deslizar o botao slider */
+    let btnSlider = document.getElementById('btnSlider1');
+    let body = document.querySelector('body');
+    
+    if(argDiaOuNoite == 'dia'){
+        //console.log('ChangeBackground: argumento dia');
+        btnSlider.checked = true;
+        body.classList.remove('fundoNoite');
+        body.classList.add('fundoDia');
+        
+    }else{
+        //console.log('ChangeBackground: argumento noite');
+        btnSlider.checked = false;
+        body.classList.remove('fundoDia');
+        body.classList.add('fundoNoite');
+    }
+}
+
+function getSliderPosition(){
+   /* Pega a posicao do botao slider (checkbox) */ 
+   objeto = document.getElementById('btnSlider1');
+    if (objeto.checked == true){
+        ChangeBackground('dia');
+        //console.log('Mexeu no slider: chamou c/ argumento dia...');
     } else {
-        console.log('background noite');
+        ChangeBackground('noite');
+        //console.log('Mexeu no slider: chamou c/ argumento noite...');
     }
         
 }
