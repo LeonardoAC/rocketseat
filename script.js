003b7eb73f62a44ca286413854b7c0fd0c0545d6
root <root@TI-Leonardo>
2023-10-10 10:58:53 -0300

    another commit

--- script.js
+++ script.js
@@ -1,4 +1,31 @@
 /**/
+
+function ChangeBackground(argDiaOuNoite){
+    /* Troca a imagem de fundo da página, ao deslizar o botao slider */
+    let btnSlider = document.getElementById('btnSlider1');
+    let body = document.querySelector('body');
+    let picture = document.getElementById('imgAvatar');
+    let box = document.getElementById('divBox');
+    
+    if(argDiaOuNoite == 'dia'){
+        //console.log('ChangeBackground: argumento dia');
+        btnSlider.checked = true;
+        body.classList.remove('fundoNoite', 'bordaNoite');
+        body.classList.add('fundoDia', 'bordaDia');
+        picture.src = "assets/avatar-light.png";
+        //Object.assign(box.style,{border: "1px solid #000000"});
+        //box.style.cssText = "border: 1px solid #000000";
+        
+    }else{
+        //console.log('ChangeBackground: argumento noite');
+        btnSlider.checked = false;
+        body.classList.remove('fundoDia');
+        body.classList.add('fundoNoite');
+        picture.src = "assets/avatar.png";
+        box.style.cssText = "border: 1px solid #ffffff";
+    }
+}
+
 function getTimeAndSetTheBackground(){
     /*  
         Se for entre 06:00 e 17:59 fundo será claro
@@ -9,35 +36,13 @@ function getTimeAndSetTheBackground(){
     // Pega a hora local
     let date = new Date();
     const hour = date.getHours();
-    console.log('Hora: '+hour);
+    //console.log('Hora: '+hour);
     if ( hour >=6 && hour <=17 ){
-        console.log('getTimeAndSetTheBackground: dia');
+        //console.log('getTimeAndSetTheBackground: dia');
         ChangeBackground('dia');
     } else {
-        console.log('getTimeAndSetTheBackground: noite');
+        //console.log('getTimeAndSetTheBackground: noite');
         ChangeBackground('noite');
-    }
-}
-
-function ChangeBackground(argDiaOuNoite){
-    /* Troca a imagem de fundo da página, ao deslizar o botao slider */
-    let btnSlider = document.getElementById('btnSlider1');
-    let body = document.querySelector('body');
-    let picture = document.getElementById('imgAvatar');
-    
-    if(argDiaOuNoite == 'dia'){
-        //console.log('ChangeBackground: argumento dia');
-        btnSlider.checked = true;
-        body.classList.remove('fundoNoite');
-        body.classList.add('fundoDia');
-        picture.src = "assets/avatar-light.png";
-        
-    }else{
-        //console.log('ChangeBackground: argumento noite');
-        btnSlider.checked = false;
-        body.classList.remove('fundoDia');
-        body.classList.add('fundoNoite');
-        picture.src = "assets/avatar.png";
     }
 }
 
